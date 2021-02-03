44a8c54fe4dd0ee5c67a740184b9b545bb9cbadc
Rain120 <chao1202016@outlook.com>
2021-02-03 17:55:17 +0800

    feat: color pool

--- packages/color/__tests__/index.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const getSomethings = require('..');
-
-describe('module', () => {
-  test('getSomethings', () => {
-    expect(getSomethings()).toBe(undefined);
-  });
-});
--- packages/color/index.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export interface IOptions {
-  [key: string]: any;
-}
-
-export default function getSomethings(root?: string, options?: IOptions | undefined) {
-  console.log(root, options);
-}
