4dbb6654c3e7c659642909d8465aaee7c8d751e1
Rain120 <chao1202016@outlook.com>
2021-02-07 10:56:29 +0800

    feat: update color pool, add annotation and link

--- packages/color-pool/src/pool.ts
+++ packages/color-pool/src/pool.ts
@@ -1,153 +1,144 @@
-/*
- * @Author: Rainy [https://github.com/rain120]
- * @Date: 2021-02-01 21:08:29
- * @LastEditors: Rainy
- * @LastEditTime: 2021-02-05 11:36:33
- */
-
+// LINK_TO: https://developer.mozilla.org/zh-CN/docs/Web/CSS/color_value
 export const colorPool = {
-  aliceblue: '#f0f8ff',
-  antiquewhite: '#faebd7',
-  aqua: '#0ff',
-  aquamarine: '#7fffd4',
-  azure: '#f0ffff',
-  beige: '#f5f5dc',
-  bisque: '#ffe4c4',
-  black: '#000',
-  blanchedalmond: '#ffebcd',
-  blue: '#00f',
-  blueviolet: '#8a2be2',
-  brown: '#a52a2a',
-  burlywood: '#deb887',
-  cadetblue: '#5f9ea0',
-  chartreuse: '#7fff00',
-  chocolate: '#d2691e',
-  coral: '#ff7f50',
-  cornflowerblue: '#6495ed',
-  cornsilk: '#fff8dc',
-  crimson: '#dc143c',
-  cyan: '#0ff',
-  darkblue: '#00008b',
-  darkcyan: '#008b8b',
-  darkgoldenrod: '#b8860b',
-  darkgray: '#a9a9a9',
-  darkgreen: '#006400',
-  darkkhaki: '#bdb76b',
-  darkmagenta: '#8b008b',
-  darkolivegreen: '#556b2f',
-  darkorange: '#ff8c00',
-  darkorchid: '#9932cc',
-  darkred: '#8b0000',
-  darksalmon: '#e9967a',
-  darkseagreen: '#8fbc8f',
-  darkslateblue: '#483d8b',
-  darkslategray: '#2f4f4f',
-  darkturquoise: '#00ced1',
-  darkviolet: '#9400d3',
-  deeppink: '#ff1493',
-  deepskyblue: '#00bfff',
-  dimgray: '#696969',
-  dodgerblue: '#1e90ff',
-  feldspar: '#d19275',
-  firebrick: '#b22222',
-  floralwhite: '#fffaf0',
-  forestgreen: '#228b22',
-  fuchsia: '#f0f',
-  gainsboro: '#dcdcdc',
-  ghostwhite: '#f8f8ff',
-  gold: '#ffd700',
-  goldenrod: '#daa520',
-  gray: '#808080',
-  green: '#008000',
-  greenyellow: '#adff2f',
-  honeydew: '#f0fff0',
-  hotpink: '#ff69b4',
-  indianred: '#cd5c5c',
-  indigo: '#4b0082',
-  ivory: '#fffff0',
-  khaki: '#f0e68c',
-  lavender: '#e6e6fa',
-  lavenderblush: '#fff0f5',
-  lawngreen: '#7cfc00',
-  lemonchiffon: '#fffacd',
-  lightblue: '#add8e6',
-  lightcoral: '#f08080',
-  lightcyan: '#e0ffff',
-  lightgoldenrodyellow: '#fafad2',
-  lightgreen: '#90ee90',
-  lightgrey: '#d3d3d3',
-  lightpink: '#ffb6c1',
-  lightsalmon: '#ffa07a',
-  lightseagreen: '#20b2aa',
-  lightskyblue: '#87cefa',
-  lightslateblue: '#8470ff',
-  lightslategray: '#789',
-  lightsteelblue: '#b0c4de',
-  lightyellow: '#ffffe0',
-  lime: '#0f0',
-  limegreen: '#32cd32',
-  linen: '#faf0e6',
-  magenta: '#f0f',
-  maroon: '#800000',
-  mediumaquamarine: '#66cdaa',
-  mediumblue: '#0000cd',
-  mediumorchid: '#ba55d3',
-  mediumpurple: '#9370d8',
-  mediumseagreen: '#3cb371',
-  mediumslateblue: '#7b68ee',
-  mediumspringgreen: '#00fa9a',
-  mediumturquoise: '#48d1cc',
-  mediumvioletred: '#c71585',
-  midnightblue: '#191970',
-  mintcream: '#f5fffa',
-  mistyrose: '#ffe4e1',
-  moccasin: '#ffe4b5',
-  navajowhite: '#ffdead',
-  navy: '#000080',
-  oldlace: '#fdf5e6',
-  olive: '#808000',
-  olivedrab: '#6b8e23',
-  orange: '#ffa500',
-  orangered: '#ff4500',
-  orchid: '#da70d6',
-  palegoldenrod: '#eee8aa',
-  palegreen: '#98fb98',
-  paleturquoise: '#afeeee',
-  palevioletred: '#d87093',
-  papayawhip: '#ffefd5',
-  peachpuff: '#ffdab9',
-  peru: '#cd853f',
-  pink: '#ffc0cb',
-  plum: '#dda0dd',
-  powderblue: '#b0e0e6',
-  purple: '#800080',
-  red: '#f00',
-  rosybrown: '#bc8f8f',
-  royalblue: '#4169e1',
-  saddlebrown: '#8b4513',
-  salmon: '#fa8072',
-  sandybrown: '#f4a460',
-  seagreen: '#2e8b57',
-  seashell: '#fff5ee',
-  sienna: '#a0522d',
-  silver: '#c0c0c0',
-  skyblue: '#87ceeb',
-  slateblue: '#6a5acd',
-  slategray: '#708090',
-  snow: '#fffafa',
-  springgreen: '#00ff7f',
-  steelblue: '#4682b4',
-  tan: '#d2b48c',
-  teal: '#008080',
-  thistle: '#d8bfd8',
-  tomato: '#ff6347',
-  transparent: '#0000',
-  turquoise: '#40e0d0',
-  violet: '#ee82ee',
-  violetred: '#d02090',
-  wheat: '#f5deb3',
-  white: '#fff',
-  whitesmoke: '#f5f5f5',
-  yellow: '#ff0',
-  yellowgreen: '#9acd32',
+  transparent: 'rgba(0, 0, 0, 0)',
+  black: '#000000', // 黑
+  silver: '#c0c0c0', // 银
+  gray: '#808080', // 灰
+  white: '#ffffff', // 白
+  maroon: '#800000', // 褐
+  red: '#ff0000', // 红
+  purple: '#800080', // 紫
+  fuchsia: '#ff00ff', // 紫红
+  green: '#008000', // 绿
+  lime: '#00ff00', // 绿黄
+  olive: '#808000', // 橄榄绿
+  yellow: '#ffff00', // 黄
+  navy: '#000080', // 藏青
+  blue: '#0000ff', // 蓝
+  teal: '#008080', // 青
+  aqua: '#00ffff', // 水绿
+  orange: '#ffa500', // 橙
+  aliceblue: '#f0f8ff', // 浅灰蓝
+  antiquewhite: '#faebd7', // 古董白
+  aquamarine: '#7fffd4', // 海蓝
+  azure: '#f0ffff', // 蔚蓝
+  beige: '#f5f5dc', // 浅褐
+  bisque: '#ffe4c4', // 橘黄
+  blanchedalmond: '#ffebcd', // 杏仁白
+  blueviolet: '#8a2be2', // 蓝紫
+  brown: '#a52a2a', // 褐
+  burlywood: '#deb887', // 原木色
+  cadetblue: '#5f9ea0', // 灰蓝
+  chartreuse: '#7fff00', // 黄绿
+  chocolate: '#d2691e', // 巧克力色
+  coral: '#ff7f50', // 珊瑚红
+  cornflowerblue: '#6495ed', // 矢车菊蓝
+  cornsilk: '#fff8dc', // 玉米穗黄
+  crimson: '#dc143c', // 深红
+  darkblue: '#00008b', // 深蓝
+  darkcyan: '#008b8b', // 深青
+  darkgoldenrod: '#b8860b', // 暗金
+  darkgreen: '#006400', // 深绿
+  darkgrey: '#a9a9a9', // 深灰
+  darkkhaki: '#bdb76b', // 暗黄褐
+  darkmagenta: '#8b008b', // 深紫
+  darkolivegreen: '#556b2f', // 深橄榄绿
+  darkorange: '#ff8c00', // 深橙
+  darkorchid: '#9932cc', // 深兰花紫
+  darkred: '#8b0000', // 深红
+  darksalmon: '#e9967a', // 深橙红
+  darkseagreen: '#8fbc8f', // 深海绿
+  darkslateblue: '#483d8b', // 暗灰蓝
+  darkslategrey: '#2f4f4f', // 墨绿
+  darkturquoise: '#00ced1', // 暗宝石绿
+  darkviolet: '#9400d3', // 深紫罗兰
+  deeppink: '#ff1493', // 深粉红
+  deepskyblue: '#00bfff', // 深天蓝
+  dimgrey: '#696969', // 暗灰
+  dodgerblue: '#1e90ff', // 遮板蓝
+  firebrick: '#b22222', // 砖红
+  floralwhite: '#fffaf0', // 花白
+  forestgreen: '#228b22', // 丛林绿
+  gainsboro: '#dcdcdc', // 浅灰
+  ghostwhite: '#f8f8ff', // 幽灵白
+  gold: '#ffd700', // 金
+  goldenrod: '#daa520', // 橘黄
+  greenyellow: '#adff2f', // 黄绿
+  grey: '#808080', // 灰
+  honeydew: '#f0fff0', // 蜜瓜色
+  hotpink: '#ff69b4', // 亮粉
+  indianred: '#cd5c5c', // 印第安红
+  indigo: '#4b0082', // 靛蓝
+  ivory: '#fffff0', // 象牙白
+  khaki: '#f0e68c', // 卡其色
+  lavender: '#e6e6fa', // 淡紫
+  lavenderblush: '#fff0f5', // 淡紫红
+  lawngreen: '#7cfc00', // 草绿
+  lemonchiffon: '#fffacd', // 粉黄
+  lightblue: '#add8e6', // 淡蓝
+  lightcoral: '#f08080', // 浅珊瑚色
+  lightcyan: '#e0ffff', // 淡青
+  lightgoldenrodyellow: '#fafad2', // 浅金黄
+  lightgreen: '#90ee90', // 浅绿
+  lightgrey: '#d3d3d3', // 浅灰
+  lightpink: '#ffb6c1', // 淡粉
+  lightsalmon: '#ffa07a', // 浅肉色
+  lightseagreen: '#20b2aa', // 浅海绿
+  lightskyblue: '#87cefa', // 浅天蓝
+  lightslategrey: '#778899', // 浅青灰
+  lightsteelblue: '#b0c4de', // 浅钢蓝
+  lightyellow: '#ffffe0', // 浅黄
+  limegreen: '#32cd32', // 酸橙绿
+  linen: '#faf0e6', // 亚麻色
+  mediumaquamarine: '#66cdaa', // 中绿
+  mediumblue: '#0000cd', // 中蓝
+  mediumorchid: '#ba55d3', // 间兰花紫
+  mediumpurple: '#9370db', // 中紫
+  mediumseagreen: '#3cb371', // 间海绿
+  mediumslateblue: '#7b68ee', // 中暗蓝
+  mediumspringgreen: '#00fa9a', // 中春绿
+  mediumturquoise: '#48d1cc', // 中海湖蓝
+  mediumvioletred: '#c71585', // 中紫罗兰
+  midnightblue: '#191970', // 午夜蓝
+  mintcream: '#f5fffa', // 薄荷乳白
+  mistyrose: '#ffe4e1', // 粉玫瑰红
+  moccasin: '#ffe4b5', // 鹿皮色
+  navajowhite: '#ffdead', // 纳瓦白
+  oldlace: '#fdf5e6', // 浅米色
+  olivedrab: '#6b8e23', // 橄榄褐
+  orangered: '#ff4500', // 橙红
+  orchid: '#da70d6', // 兰花紫
+  palegoldenrod: '#eee8aa', // 灰菊黄
+  palegreen: '#98fb98', // 苍绿
+  paleturquoise: '#afeeee', // 苍宝石绿
+  palevioletred: '#db7093', // 苍紫罗兰
+  papayawhip: '#ffefd5', // 木瓜色
+  peachpuff: '#ffdab9', // 桃色
+  peru: '#cd853f', // 秘鲁色
+  pink: '#ffc0cb', // 粉
+  plum: '#dda0dd', // 李子色
+  powderblue: '#b0e0e6', // 粉蓝
+  rosybrown: '#bc8f8f', // 玫瑰粽
+  royalblue: '#4169e1', // 宝蓝
+  saddlebrown: '#8b4513', // 马鞍棕
+  salmon: '#fa8072', // 鲑肉色
+  sandybrown: '#f4a460', // 沙褐色
+  seagreen: '#2e8b57', // 海绿
+  seashell: '#fff5ee', // 贝壳白
+  sienna: '#a0522d', // 赭
+  skyblue: '#87ceeb', // 天蓝
+  slateblue: '#6a5acd', // 青蓝
+  slategrey: '#708090', // 青灰
+  snow: '#fffafa', // 雪白
+  springgreen: '#00ff7f', // 春绿
+  steelblue: '#4682b4', // 铁青
+  tan: '#d2b48c', // 棕褐
+  thistle: '#d8bfd8', // 苍紫
+  tomato: '#ff6347', // 番茄红
+  turquoise: '#40e0d0', // 蓝绿
+  violet: '#ee82ee', // 紫罗兰色
+  wheat: '#f5deb3', // 麦色
+  whitesmoke: '#f5f5f5', // 烟白
+  yellowgreen: '#9acd32', // 黄绿
+  rebeccapurple: '#663399', // 利百加紫
 };
