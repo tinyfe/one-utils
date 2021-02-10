934826258b171914d3b7d776a41e1d8db5ee354b
Rain120 <chao1202016@outlook.com>
2021-02-10 11:23:32 +0800

    feat: rollup build for single pkg; remove color-keywords useless jest config;

--- rollup.config.js
+++ rollup.config.js
@@ -7,15 +7,20 @@ import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 
-const { NODE_ENV } = process.env;
+let { NODE_ENV, BUILD_PATH } = process.env;
+
+if (typeof BUILD_PATH === 'string') {
+  BUILD_PATH = BUILD_PATH.split(';');
+}
 
 const pkgsRoot = path.join(__dirname, 'packages');
 const pkgs = fs
   .readdirSync(pkgsRoot)
+  .filter(dir => BUILD_PATH.includes(dir))
   .map(dir => path.join(pkgsRoot, dir))
   .map(location => {
     return {
-      location: location,
+      location,
       pkgJson: require(path.resolve(location, 'package.json')),
     };
   });
