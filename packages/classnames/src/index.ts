e5a5ab215c304cfe357aebb0382f7a822a07716a
Rain120 <chao1202016@outlook.com>
2021-04-01 15:20:16 +0800

    fix: prefix classnames Array bug

--- packages/classnames/src/index.ts
+++ packages/classnames/src/index.ts
@@ -1,34 +1,40 @@
+// LINK_TO: https://github.com/JedWatson/classnames
+const hasOwn = {}.hasOwnProperty;
+
 function prefix(name: string = ''): (...args: any[]) => string {
   return (...args: any): string => {
-    const hasOwn = {}.hasOwnProperty;
-
     const classes: string[] = [];
 
     for (let i = 0; i < args.length; i++) {
       const arg = args[i];
+
+      if (!arg) {
+        continue;
+      }
+
       const argType = typeof arg;
 
       if (argType === 'string' || argType === 'number') {
-        classes.push(name + arg);
+        classes.push(arg);
       } else if (Array.isArray(arg)) {
         if (arg.length) {
-          const inner = classnames.apply(null, arg);
-          inner && classes.push(name + inner);
+          const inner = prefix(name).apply(null, arg);
+          inner && classes.push(inner);
         }
       } else if (argType === 'object') {
         if (arg.toString !== Object.prototype.toString) {
-          classes.push(name + arg.toString());
+          classes.push(arg.toString());
         } else {
           for (const key in arg) {
             if (hasOwn.call(arg, key) && arg[key]) {
-              classes.push(name + key);
+              classes.push(key);
             }
           }
         }
       }
     }
 
-    return classes.join(' ');
+    return classes.map(c => ((c + '').includes(name) ? c : name + c)).join(' ');
   };
 }
 
