925f20da8ee152d11aafe21bb8585b9a9755d53d
Rain120 <1085131904@qq.com>
2021-11-21 19:46:46 +0800

    feat(download-git-repo): remove test case

--- packages/download-git-repo/__tests__/index.test.ts
+++ packages/download-git-repo/__tests__/index.test.ts
@@ -6,16 +6,16 @@ describe('@tinyfe/download-git-repo', () => {
   jest.setTimeout(2000);
 
   test('test', () => {
-    (async () => {
-      await downloadPromisify(
-        'Rain120/mini-webpack',
-        './packages/download-git-repo/__tests__/clone/',
-        {
-          clone: true,
-        },
-      ).catch(err => {
-        console.log(err);
-      });
-    })();
+    // (async () => {
+    //   await downloadPromisify(
+    //     'Rain120/mini-webpack',
+    //     './packages/download-git-repo/__tests__/clone/',
+    //     {
+    //       clone: true,
+    //     },
+    //   ).catch(err => {
+    //     console.log(err);
+    //   });
+    // })();
   });
 });
